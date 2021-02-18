35cf0e2c6626127bd6d3ba171fd685148f4fa62d
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-02-18 16:48:27 +0300

    React state  and lifestyle

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -1,33 +1,36 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-/*import todolist file*/
 
-//import TodoList from "./TodoList";
-
-const name = "Gilbert Perez";
-function tick() {
-
-
-const element = (
-    <div>
-    <h1>Hello, {name}</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-        );
-
-var destination = document.querySelector("#container");
-
-ReactDom.render(
-    <div>
-
-        {element}
-
-
-
-    </div>,
-    destination
-);
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
     }
+componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+}
+componentWillMount() {
+        clearInterval(this.timerID);
+}
+tick(){
+        this.setState({
+            date: new Date()
+        });
+}
 
-        setInterval(tick, 1000);
+    render() {
+        return (
+            <div>
+                <h1>Hello, world</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+ReactDom.render(
+    <Clock />,
+    document.getElementById('container')
+);
