d91dde64af90c96779d5af375f699d21ac49c36d
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-02-18 15:12:53 +0300

    JSX function call

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -8,12 +8,18 @@ import "./index.css";
 const name = "Gilbert Perez";
 const element = <h1>Hello, {name}</h1>;
 
-
+    function formatName(user){
+        return user.firstName + ' ' + user.lastName;
+    }
+const user = {
+        firstName: 'Gilbert',
+    lastName: 'pere'
+};
 var destination = document.querySelector("#container");
 
 ReactDom.render(
     <div>
-        <p>Hello world!</p>
+        <p>Hello world! {formatName(user)}</p>
         {element}
     </div>,
     destination
