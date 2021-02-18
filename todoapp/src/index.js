139b5905031a85c08c6be1271540fa587b500783
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-02-18 15:09:11 +0300

    jsx

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -3,7 +3,10 @@ import ReactDom from "react-dom";
 import "./index.css";
 /*import todolist file*/
 
-import TodoList from "./TodoList";
+//import TodoList from "./TodoList";
+
+const name = "Gilbert Perez";
+const element = <h1>Hello, {name}</h1>;
 
 
 var destination = document.querySelector("#container");
@@ -11,7 +14,7 @@ var destination = document.querySelector("#container");
 ReactDom.render(
     <div>
         <p>Hello world!</p>
-        <TodoList/>
+        {element}
     </div>,
     destination
 );
